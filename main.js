c0fe79302ec78480664daee0ba192915759a4bb6
victorsworld <victordirect48@gmail.com>
2023-02-23 10:55:15 -0500

    FINISHED

--- main.js
+++ main.js
@@ -2,6 +2,56 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+function isAdmin(user){
+    if(user.userRole !== "ADMIN"){
+        return false;
+    } else{ return true; }
+}
+
+function getEmail(user){
+let a =`${user.firstName.toLowerCase()}.${user.lastName.toLowerCase()}@codeimmersives.com`
+return a;
+}
+
+function getPlaylistLength(pL){
+   return pL.songs.length; 
+}
+
+function getHardestHomework(hw){
+    if(hw.length === 0){
+        return "";
+    }
+        min = hw[0]
+
+        for (let i = 0; i < hw.length; i++) {   
+        if(hw[i].averageScore < min.averageScore)  
+           min = hw[i] }
+           
+    
+    return min.name;
+}
+
+function createPhonebook(array1, array2){
+    let a = {};
+    for(let i = 0; i < array1.length; i ++){
+        a[array1[i]] = array2[i];
+
+    }
+    
+    return a;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // ┌─────────────────────────────────────┐
